e1a59ee310c68149007d016a50fedd77f1788ffc
ventesh surti <61005846+ventesh@users.noreply.github.com>
2023-06-20 17:54:43 +0530

    Add files via upload

---
+++ theme.config.js
@@ -0,0 +1,11 @@
+const YEAR = new Date().getFullYear()
+
+export default {
+  footer: (
+    <footer>
+      <small>
+        <time>{YEAR}</time> © Ventesh Surti
+      </small>
+    </footer>
+  ),
+}
